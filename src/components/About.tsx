15bd7d215726374cce4b550d0c5a40a1013572bf
anandb104 <210169445+anandb104@users.noreply.github.com>
2026-08-13 21:07:58 +0530

    Add the about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,29 @@
 import bgimage from "../assets/bgimg.jpg";
+import { Button } from "@/components/ui/button"
+import bru from "../assets/bru.png";
+import nescafe from "../assets/nescafe.png";
+import continental from "../assets/continental.png";
+import logo from "../assets/logo.png";
 
-export default function About(){
+export default function Home(){
     return(
         <div className="flex flex-1 gap-15 flex-col bg-cover bg-center text-4xl items-center justify-center text-black font-['Black_Ops_One'] pl-2.5 " style={{backgroundImage:`url(${bgimage})`}}>
+            <div className="mt-10 flex gap-10 flex-col justify-center items-center">
+            <h1>About Us</h1>
+
+Welcome to our online store, where shopping is made simple, convenient, and enjoyable. We bring together a collection of quality products across different categories, making it easy for you to discover something you love.
+
+Our goal is to provide a smooth shopping experience where you can browse products, compare prices, add your favorites to your cart, and shop with ease. Whether you're looking for everyday essentials or something new to try, we're here to make your shopping experience simple and hassle-free.
+
+Why Shop With Us?
+Wide Selection – Discover a variety of products in one place.
+Simple Shopping – Browse, add products to your cart, and manage your order easily.
+Great Prices – Find products at prices you'll appreciate.
+Easy Experience – We focus on keeping shopping straightforward and enjoyable.
+
+Shop. Discover. Enjoy.
+
+</div>
         </div>
-    );
+    )
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,11 +8,6 @@ import logo from "../assets/logo.png";
 export default function Home(){
     return(
         <div className="flex flex-1 gap-15 flex-col bg-cover bg-center text-4xl items-center justify-center text-black font-['Black_Ops_One'] pl-2.5 " style={{backgroundImage:`url(${bgimage})`}}>
-            {/* <div className="flex gap-40 absolute top-30 ml-5">
-            <img src={bru} className="  h-55"></img>
-            <img src={nescafe} className="  h-55"></img>
-            <img src={continental} className="  h-55"></img>
-            </div> */}
             <div className="mt-10 flex gap-10 flex-col">
         Shop. <br/>Discover.<br/> Enjoy.<br/>
 Find products you'll love at prices you'll appreciate. Browse our collection, discover new favorites, and make every shopping experience simple, convenient, and enjoyable.
